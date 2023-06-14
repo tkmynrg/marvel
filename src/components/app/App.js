7cfe35512d8392adb415972bbf852dcce59aa425
tkmynrg <dlsenok@mail.ru>
2023-06-15 01:15:43 +0300

    react router v5 [3]

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,9 @@
-import {useState} from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+import {MainPage, ComicsPage} from '../pages'
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-import decoration from '../../resources/img/vision.png';
-import AppBanner from "../appBanner/AppBanner";
 
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id)
-    }
-
 
     return (
         <Router>
@@ -27,22 +12,10 @@ const App = () => {
                 <main>
                     <Switch>
                         <Route exact path="/">
-                            <ErrorBoundary>
-                                <RandomChar/>
-                            </ErrorBoundary>
-                            <div className="char__content">
-                                <ErrorBoundary>
-                                    <CharList onCharSelected={onCharSelected}/>
-                                </ErrorBoundary>
-                                <ErrorBoundary>
-                                    <CharInfo charId={selectedChar}/>
-                                </ErrorBoundary>
-                            </div>
-                            <img className="bg-decoration" src={decoration} alt="vision"/>
+                            <MainPage></MainPage>
                         </Route>
                         <Route exact path="/comics">
-                            <AppBanner/>
-                            <ComicsList/>
+                            <ComicsPage></ComicsPage>
                         </Route>
                     </Switch>
                 </main>
